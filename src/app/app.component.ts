1b59bdf5c2926a66b09c8e41843d446eb2889a59
Dorian Mailhe <mailhedori@eisti.eu>
2022-10-09 22:06:40 +0200

    markdown preview editor, more tests and gh actions configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world';
+  title: string = 'hello-world';
+  markdown: string = '';
+
+  constructor() {}
+
+  public multiplyFunction(a: number, b: number): number {
+    return a * b;
+  }
 }
