b768a43470783eb4929271fe1e9bee9ce735e8ad
Dorian Mailhe <mailhedori@eisti.eu>
2022-10-17 18:28:43 +0200

    release 1.0 pause

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { compareAsc, startOfTomorrow } from 'date-fns';
+import { compareDesc, startOfTomorrow } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,13 @@ import { compareAsc, startOfTomorrow } from 'date-fns';
 })
 export class AppComponent {
   title: string = 'hello-world';
+  isBusinessDay: boolean;
+  timer: string = '';
 
   constructor() {
-    this.getNextPause();
+    let date = new Date();
+    this.isBusinessDay = date.getDay() !== 0 && date.getDay() !== 6;
+    this.countdown();
   }
 
   public multiplyFunction(a: number, b: number): number {
@@ -19,60 +23,61 @@ export class AppComponent {
 
   public getNextPause() {
     let now = new Date();
-    let firstpause = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 30, 0);
-    let lastpause = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 30, 0);
-    let isBusinessDay = now.getDay() !== 0 && now.getDay() !== 6;
+    let firstpause = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      10,
+      30,
+      0
+    );
+    let lastpause = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      15,
+      30,
+      0
+    );
     let secondsUntilPause = 0;
     let isPause = false;
-    let cpt = compareAsc(now, firstpause) + compareAsc(now, lastpause);
-      switch (cpt) {
-        case 2:
-          secondsUntilPause = (firstpause.getTime() - now.getTime()) / 1000;
-          break;
-        case 0:
-          secondsUntilPause = (lastpause.getTime() - now.getTime()) / 1000;
-          break;
-        case -2:
-          secondsUntilPause = ((startOfTomorrow().getTime() - now.getTime()) / 1000) +  37800;
-          break;
-        default:
-          isPause = true;
-          break;
-      }
-
-      console.log(firstpause, lastpause,isBusinessDay);
+    let cpt = compareDesc(now, firstpause) + compareDesc(now, lastpause);
+    switch (cpt) {
+      case 2:
+        secondsUntilPause = firstpause.getTime() - now.getTime();
+        console.log('2');
+        break;
+      case 0:
+        secondsUntilPause = lastpause.getTime() - now.getTime();
+        console.log('0');
+        break;
+      case -2:
+        secondsUntilPause =
+          startOfTomorrow().getTime() - now.getTime() + 37800000;
+        console.log('-2');
+        break;
+      default:
+        isPause = true;
+        break;
     }
 
-  public countdown(){
-    var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-  }
+    return secondsUntilPause;
   }
 
+  public countdown() {
+    const x = setInterval(() => {
+      const distance = Math.abs(this.getNextPause());
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+      this.timer = hours + 'h ' + minutes + 'm ' + seconds + 's ';
 
+      if (distance < 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }
+}
